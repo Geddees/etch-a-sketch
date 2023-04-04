dc99f9264c8d7e3c23b311b46236450998e7e4f3
Nick Mitchell <nickmitchell517@gmail.com>
2023-04-03 18:19:29 -0700

    Add createSquare functionality using a usr prompt still

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const container = document.querySelector(".main-container");
 
 function createSquares (input) {
-
-    for (let i = 0; i < input; i++) {
+    let inputTot = input**2
+    for (let i = 0; i < inputTot; i++) {
         let square = document.createElement("div");
         square.classList.add("grey")
+        square.style.width = `calc(100%/${input})`;
         container.appendChild(square);
     }
 }
