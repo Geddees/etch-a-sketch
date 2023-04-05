5030be2c546d0b06456e88be0da66f6508a276d1
Nick Mitchell <nickmitchell517@gmail.com>
2023-04-04 20:53:23 -0700

    Add a button to create a grid using prompt

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const container = document.querySelector(".main-container");
+const button = document.getElementById("grid-maker");
+
+button.addEventListener("click", createSquares);
 
 function drawSketch () {
     this.style.backgroundColor = "black";
 }
 
 function createSquares (input) {
-    let inputTot = input**2
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    input = prompt("What grid size would you like? ");
+    let inputTot = input**2;
+
     for (let i = 0; i < inputTot; i++) {
         let square = document.createElement("div");
         square.classList.add("grey");
@@ -15,4 +24,3 @@ function createSquares (input) {
     }
 }
 
-createSquares(prompt("Hello"));
