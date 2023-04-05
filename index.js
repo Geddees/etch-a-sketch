39e1403d5e8689af99a2e1d3851f9d7dc745f03b
Nick Mitchell <nickmitchell517@gmail.com>
2023-04-04 19:33:17 -0700

    Add onmouseover effect to change the squares black

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const container = document.querySelector(".main-container");
 
+function drawSketch () {
+    this.style.backgroundColor = "black";
+}
+
 function createSquares (input) {
     let inputTot = input**2
     for (let i = 0; i < inputTot; i++) {
         let square = document.createElement("div");
-        square.classList.add("grey")
+        square.classList.add("grey");
         square.style.width = `calc(100%/${input})`;
+        square.addEventListener("mouseover", drawSketch);
         container.appendChild(square);
     }
 }
-createSquares(prompt("ENTER: "));
+
+createSquares(prompt("Hello"));
